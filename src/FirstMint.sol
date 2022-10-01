68c97d1aa7171363427a7a2c47e0c77657e5f7c6
christianturner <christianturner@hey.com>
2022-10-01 15:20:16 -0400

    vars

--- src/FirstMint.sol
+++ src/FirstMint.sol
@@ -2,3 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FirstMintContract is ERC721, Ownable {
+    uint256 public mintPrice = 0.05 ether;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    bool public isMintEnabled;
+    mapping(address => uint256) public mintedWallets;
+}
